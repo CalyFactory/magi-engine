0945792194fe91f7383b29acb7fc0aa16d3ba8ba
JSpiner <jspiner@naver.com>
2017-05-30 22:21:40 +0900

    add compare function

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-from reco import Reco 
+from reco import Reco
+from common import db_manager
+from common.util import utils
 import json
+import logging
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 with open('./jsonData.json') as conf_json:
     jsonData = json.load(conf_json)
-
-
+        
 recoModule = Reco(jsonData, None)
 
 filteredList = recoModule.getFilteredList()
 
-for listItem in filteredList:
-    print(listItem)
-
 print("=====================")
-print(recoModule.getRecoList())
+print("filtered list")
+print("=====================")
+i=0
+for listItem in filteredList:
+
+    print(
+        "[%d] %s %s"
+        %
+        (
+            i,
+            listItem['region'],
+            listItem['title']
+        )
+    )
+    i+=1
+print("\n\n\n")
+print("=====================")
+print("sorted list")
+print("=====================")
+sortedList = recoModule.getRecoList()
+i=0
+for listItem in sortedList:
+
+    print(
+        "[%d] %s %s"
+        %
+        (
+            i,
+            listItem['region'],
+            listItem['title']
+        )
+    )
+    i+=1
--- reco.py
+++ reco.py
@@ -10,6 +10,13 @@ class Reco:
         self.jsonData = jsonData
         self.userInfo = userInfo
 
+        self.initData()
+
+    def initData(self):
+        self.locationPriorityList = {}
+        for locationData in self.jsonData['locations']:
+            self.locationPriorityList[locationData['region']] = locationData['no']
+
     def getRecoList(self):
         filteredList = self.getFilteredList()
         sortedList = self.sortListByScore(filteredList)
@@ -18,16 +25,39 @@ class Reco:
         return filteredList
 
     def getFilteredList(self):
-        print("filtering")
 
         locationFilteredList = self.__getLocationFilteredList()
-        timeFilteredList = self.__getTimeFilteredList(locationFilteredList)
-        typeFilteredList = self.__getTypeFilteredList(timeFilteredList)
+#        timeFilteredList = self.__getTimeFilteredList(locationFilteredList)
+        typeFilteredList = self.__getTypeFilteredList(locationFilteredList)
 
         return typeFilteredList
 
-    #두 객체를 비교하는 함수
-    def compareRank(self, objA, objB):
+    #두 객체의 우선순위 비교하는 함수
+    #첫 번째 인자의 우선순위가 높을경우 True, 아닐경우 False 를 리턴함
+    def isFirstArgHighPriority(self, itemA, itemB):
+
+        #region
+        itemARegionPriority = self.locationPriorityList[itemA['region']]
+        itemBRegionPriority = self.locationPriorityList[itemB['region']]
+
+        if itemARegionPriority < itemBRegionPriority:
+            return False
+        elif itemARegionPriority > itemBRegionPriority:
+            return True
+
+        #목적지표 event_type_availability
+        ## TODO : 회의때 목적은 여러개인데 목적지표로 순위를 정하는것을 목적이 하나인 경우만 생각해서 정한 것 같다. 고민이 필요함
+
+        eventTypeId = self.jsonData['eventType'][0]['typeId']
+
+        itemAAvailabilityScore = itemA['event_type_availability'][eventTypeId]['int'] * 2 + itemA['event_type_availability'][eventTypeId]['after']
+        itemBAvailabilityScore = itemB['event_type_availability'][eventTypeId]['int'] * 2 + itemB['event_type_availability'][eventTypeId]['after']
+
+        if itemAAvailabilityScore < itemBAvailabilityScore:
+            return False 
+        elif itemARegionPriority > itemBRegionPriority:
+            return True 
+
         return True
 
     def sortListByScore(self, originList):
@@ -42,18 +72,22 @@ class Reco:
 
         #목적지표가 아직 db에 없기에 랜덤으로 만들어주는 부분
         for originData in originList:
-            originData['type_able_ing'] = random.choice([True, False])
-            originData['type_able_after'] = random.choice([True, False])
-
+            originData['event_type_availability'] = {}
+            for i in range(0,9):
+                originData['event_type_availability'][i] = {
+                    'ing' :random.choice([True, False]),
+                    'after' :random.choice([True, False])
+                }
+            print(originData)
+        
         #score 계산
-        featureList = []
         for originData in originList:
             originData['score'] = random.random()
         
         #정렬 
         for i in range(0, len(originList)):
             for j in range(i, len(originList)):
-                if self.compareRank(originList[i], originList[j]):
+                if self.isFirstArgHighPriority(originList[i], originList[j]):
                     tmp = originList[i]
                     originList[i] = originList[j]
                     originList[j] = tmp
@@ -65,14 +99,13 @@ class Reco:
     def __getLocationFilteredList(self):
         locationList = self.jsonData['locations']
         
-        # region에 입력된 값이 db에 존재하는지 체크해야하지 않을까?
+        # TODO : region에 입력된 값이 db에 존재하는지 체크해야하지 않을까?
         queryOptionParam = ", ".join("'%s'" % locationData['region'] for locationData in locationList)
-        print(queryOptionParam)
 
         result = utils.fetch_all_json(
             db_manager.query(
                 """
-                SELECT reco_hashkey 
+                SELECT reco_hashkey, region, title 
                 FROM RECOMMENDATION
                 WHERE
                     region IN (%s) 
@@ -92,7 +125,6 @@ class Reco:
         return originList
 
 def hello():
-    print("hello")
     result = utils.fetch_all_json(
         db_manager.query(
             """
@@ -103,4 +135,3 @@ def hello():
         )
     )
 
-    print((result))
